8b3a22c124e893bd0084ad902d098dd32d7333dd
jacqueline <jacqueline@jacquelinesmbp2.home>
2014-04-19 17:35:29 +0200

    update

--- WebContent/navigation.js
+++
@@ -1,7 +0,0 @@
-function Toggle( id )
-{
-if( document.getElementById( id ).style.display == 'none' )
-document.getElementById( id ).style.display = '';
-else
-document.getElementById( id ).style.display = 'none';
-}
